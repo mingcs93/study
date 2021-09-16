21f91baf5c3ccab7c9afa9d0c1707487947396d4
mingcs <1350011072@qq.com>
2021-09-16 08:11:27 +0800

    merge sort 0916

--- merge_sort/main.cpp
+++ merge_sort/main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <string>
+#include "merge_sort.hpp"
+
+struct Student
+{
+	std::string name;
+	int16_t age;
+
+	friend bool operator<= (const Student& l, const Student& r)
+	{
+		if (l.age <= r.age) return true;
+		return false;
+	}
+
+	friend std::ostream& operator<<(std::ostream& output, const Student& stu)
+	{
+		output << "(" << "name: " << stu.name << ", " << "age: " << stu.age << ")";
+		return output;
+	}
+};
 
 int main(int argc, char** argv)
 {
+	{
+		MergeSort<int32_t> mySort{ std::initializer_list<int>{5, 4, 7, 3, 10, 2, 7, 9, 32} };
+		mySort.sort();
+		mySort.show();
+	}
+
+	{
+		Student stu1;
+		stu1.name = "zhangsan";
+		stu1.age = 20;
+
+		Student stu2;
+		stu2.name = "jack";
+		stu2.age = 10;
+
+		Student stu3;
+		stu3.name = "tom";
+		stu3.age = 24;
+
+		MergeSort<Student> mySort{ std::initializer_list<Student>{stu1, stu2, stu3} };
+		mySort.sort();
+		mySort.show();
+	}
 	return 0;
 }
--- merge_sort/merge_sort.hpp
+++ merge_sort/merge_sort.hpp
@@ -1,6 +1,68 @@
 #pragma once
+#include <iostream>
+#include <vector>
 
+template<typename T>
 class MergeSort
 {
+public:
+	MergeSort(std::initializer_list<T>& values)
+		: m_nums(values.begin(), values.end())
+	{
+	}
 
+	void sort()
+	{
+		process(0, m_nums.size() - 1);
+	}
+
+	void show()
+	{
+		for (auto& item : m_nums)
+		{
+			std::cout << item << " ";
+		}
+		std::cout << std::endl;
+	}
+
+private:
+	void process(size_t left, size_t right)
+	{
+		if (left == right) return;
+
+		size_t mid = left + ((right - left) >> 1);
+		process(left, mid);
+		process(mid + 1, right);
+		merge(left, mid, right);
+	}
+
+	void merge(size_t lelt, size_t mid, size_t right)
+	{
+		std::vector<T> tmp(right-lelt+1);
+		size_t idx = 0;
+		size_t p1 = lelt;
+		size_t p2 = mid + 1;
+		while (p1 <= mid && p2 <= right) 
+		{
+			tmp.at(idx++) = m_nums.at(p1) <= m_nums.at(p2) ? m_nums.at(p1++) : m_nums.at(p2++);
+		}
+
+		while (p1 <= mid)
+		{
+			tmp.at(idx++) = m_nums.at(p1++);
+		}
+
+		while (p2 <= right)
+		{
+			tmp.at(idx++) = m_nums.at(p2++);
+		}
+
+		for (auto& item : tmp)
+		{
+			m_nums.at(lelt++) = item;
+		}
+	}
+
+private:
+	std::vector<T> m_nums;
 };
